a1777159b0892d7f9e89e2f9e4a9368fddb002eb
gev414 <georgiev98hws@gmail.com>
2026-07-13 14:26:15 +0300

    initialize Biohazard NeoForge mod

---
+++ src/main/java/io/github/gev414/biohazard/Biohazard.java
@@ -0,0 +1,12 @@
+package io.github.gev414.biohazard;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Biohazard.MOD_ID)
+public final class Biohazard {
+    public static final String MOD_ID = "biohazard";
+
+    public Biohazard(IEventBus modEventBus) {
+    }
+}
