eb226de4f7aa1cf9e87749a801d7b3034d56c2ad
gev414 <georgiev98hws@gmail.com>
2026-07-13 17:55:56 +0300

    add custom Brute texture

--- src/main/java/io/github/gev414/biohazard/client/renderer/BruteRenderer.java
+++ src/main/java/io/github/gev414/biohazard/client/renderer/BruteRenderer.java
@@ -4,14 +4,26 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.gev414.biohazard.entity.BruteEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.monster.Zombie;
 
 public final class BruteRenderer extends ZombieRenderer {
+
+    private static final ResourceLocation BRUTE_TEXTURE =
+            ResourceLocation.fromNamespaceAndPath(
+                    "biohazard",
+                    "textures/entity/brute.png"
+            );
 
     public BruteRenderer(EntityRendererProvider.Context context) {
         super(context);
 
         this.shadowRadius *= BruteEntity.SCALE;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Zombie entity) {
+        return BRUTE_TEXTURE;
     }
 
     @Override
